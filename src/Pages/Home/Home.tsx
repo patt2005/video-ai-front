eb2768da6e2842cd09562787ebb74d23f2edae37
silviu <silviucotelea8@gmail.com>
2026-02-04 20:31:48 +0200

    am adaugat light/night theme si am adaugat un scrollbar

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="page-wrapper">
             <h1 className="page-title">Home</h1>
-            <p style={{ color: '#9ca3af' }}>
+            <p className="home-intro">
                 Aceasta este pagina principală a aplicației.
             </p>
         </div>
